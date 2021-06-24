75ee76afbcb2edefdeb6b5d5dd7776ceee87dcc8
RDXRC <hackerguru75@gmail.com>
2021-06-25 01:19:31 +0530

    RDX Bot
    BOT For RDX : )

---
+++ config.js
@@ -0,0 +1,15 @@
+exports.TOKEN = 'ODU2Nzg2NDkyMDQ3NjIyMTU0.YNGGmg.0-1lWRrjYc9Yb3270yP1kRBKgQ4';
+
+exports.PREFIX = '*';
+
+exports.GOOGLE_API_KEY = '';
+
+exports.GENIUS_API_KEY = '';
+
+exports.news_API = 'd78b55988c0c4e28bb3660e94c78548c';
+
+exports.giphy_API = 'fW9OKGY8T1MUhM50MFCny0vtGeiKd20y';
+
+exports.AME_API ='bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
+
+exports.blague_API ='';
